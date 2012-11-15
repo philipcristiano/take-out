843d53e97a774a56a2b993b5c8ec4f6df90d010a
Philip Cristiano <philipcristiano@gmail.com>
2012-11-14 20:08:33 -0500

    Provision a role that is the group name

--- takeout/__main__.py
+++ takeout/__main__.py
@@ -12,12 +12,29 @@ fabric.api.env.use_ssh_config = True
 @cli.app.CommandLineApp
 def takeout(app):
     if 'list' in app.params:
+        #list_nodes()
         list_nodes()
 
 def rdns_from_ip_list(iplist):
     for ip in iplist:
         if ip['ISPUBLIC']:
             return ip['RDNS_NAME']
+
+def libcloud_list_nodes():
+    from libcloud.compute.types import Provider
+    from libcloud.compute.providers import get_driver
+    import libcloud.security
+    libcloud.security.VERIFY_SSL_CERT = False
+
+    key = os.environ.get('LINODE_API_KEY')
+    linode_driver = get_driver(Provider.LINODE)(key)
+
+    for node in linode_driver.list_nodes():
+        from pprint import pprint
+        pprint(node)
+        pprint(dir(node))
+        print node.extra
+        print node.size
 
 def list_nodes():
     api = linode.api.Api()
@@ -26,7 +43,10 @@ def list_nodes():
         from pprint import pprint
         pprint(node)
         host = rdns_from_ip_list(api.linode_ip_list(LinodeID=node['LINODEID']))
-        print fabric.tasks.execute(tasks.run_ls, host=host)
+
+        run_list = 'role[{}]'.format(node['LPM_DISPLAYGROUP'])
+        #run_list = '",\n"'.join(['recipe[nginx]', 'recipe[philipcristiano]'])
+        print fabric.tasks.execute(tasks.run_chef, host=host, run_list=run_list)
 
 
 takeout.add_param("-l", "--list", help="list nodes", default=False, action="store_true")
--- takeout/tasks.py
+++ takeout/tasks.py
@@ -1,5 +1,25 @@
-import fabric.api
+import json
 
+from fabric.api import run, sudo, put
+from fabric.contrib.project import rsync_project
+from fabric.contrib.files import upload_template
+
+
+def bootstrap():
+    sudo('apt-get install rubygems')
+    return sudo('gem install chef')
+
+def run_chef(run_list):
+    sudo('mkdir -p /etc/chef')
+    put('chef-solo.rb', '/etc/chef/solo.rb', use_sudo=True)
+
+    sudo('mkdir -p /var/chef')
+    #put('attributes.json', '/etc/chef/attributes.json', use_sudo=True)
+    #sudo('echo {0} > /etc/chef/attributes.json'.format(json.dumps(attributes)))
+    upload_template('attributes.json.jinja2', '/etc/chef/attributes.json', context=dict(run_list=run_list), use_jinja=True, use_sudo=True)
+    rsync_project('/var/chef-solo', 'chef/')
+
+    sudo('chef-solo -c /etc/chef/solo.rb -j /etc/chef/attributes.json')
 
 def run_ls():
-    return fabric.api.run('ls')
+    return run('ls')
